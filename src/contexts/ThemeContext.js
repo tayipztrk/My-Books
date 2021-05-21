5d39112a571c6eaebafb6a0991f7b1d10d72e908
Tayipztrk <tayipztrk@icloud.com>
2021-05-21 11:34:01 +0300

    ilk

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const ThemeContext = React.createContext();
+
+class ThemeContextProvider extends React.Component {
+
+    state ={
+        isDarkTheme : false,
+        dark: {bg: '#222529', txt:'#D65f5f', hover: 'rgba(231, 76 , 60, 0.8)'},
+        light: {bg: '#f8f9fa', txt:'#222529', hover: 'rgba(254, 209 , 54, 0.8)'}
+    }
+
+    changeTheme = () => {
+        this.setState({isDarkTheme : !this.state.isDarkTheme})
+    }
+
+    render(){
+        return (
+            <ThemeContext.Provider value = {{...this.state, changeTheme: this.changeTheme}}> 
+            {this.props.children}
+            </ThemeContext.Provider>
+        )
+
+    }
+}
+
+export default ThemeContextProvider;
